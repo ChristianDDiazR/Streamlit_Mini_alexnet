d97ddff7c623892d1abd21886ac0b3ed2334b00d
ChristianDDiazR <christian.diaz2101@alumnos.ubiobio.cl>
2025-06-17 23:21:17 -0400

    Update train.py

--- train.py
+++ train.py
@@ -8,13 +8,11 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix, classification_report
 import seaborn as sns
 
-# Rutas
 train_dir = 'data/afhq/train'
 val_dir = 'data/afhq/val'
 img_size = (224, 224)
 batch_size = 32
 
-# Generadores de datos
 datagen = ImageDataGenerator(rescale=1./255)
 
 train_generator = datagen.flow_from_directory(
@@ -33,7 +31,6 @@ val_generator = datagen.flow_from_directory(
     shuffle=False
 )
 
-# Mini-AlexNet
 model = Sequential([
     Conv2D(32, (3,3), activation='relu', input_shape=(224,224,3)),
     MaxPooling2D(2,2),
@@ -50,27 +47,23 @@ model = Sequential([
     Flatten(),
     Dense(512, activation='relu'),
     Dropout(0.5),
-    Dense(3, activation='softmax')  # 3 clases: cat, dog, wild
+    Dense(3, activation='softmax')
 ])
 
-# Compilar modelo
 model.compile(
     optimizer=Adam(learning_rate=1e-4),
     loss='categorical_crossentropy',
     metrics=['accuracy']
 )
 
-# Entrenar modelo
 history = model.fit(
     train_generator,
     epochs=15,
     validation_data=val_generator
 )
 
-# Guardar modelo
 model.save('models/mini_alexnet_model.h5')
 
-# Evaluar rendimiento
 val_generator.reset()
 preds = model.predict(val_generator)
 y_pred = np.argmax(preds, axis=1)
